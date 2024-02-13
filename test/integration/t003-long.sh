defaea32360bf7aafb66484ab70e4fea6f139d2f
Kira Oakley <kira@eight45.net>
2024-02-13 09:48:39 -0800

    Add long integration tests.

--- test/integration/t003-long.sh
+++ test/integration/t003-long.sh
@@ -4,9 +4,21 @@
 echo '# long msg exchange (Node.js initiator)'
 ######################################################################
 TEMP=$(mktemp)
-ok 'run command' 'npx dupsh -- "./cable.rs/target/release/examples/cli 11111111111111111111111111111111 responder \"$(cat aus)\"" "node ../../dist/examples/cli 3131313131313131313131313131313131313131313131313131313131313131 initiator \"$(cat aus)\"" 2> $TEMP'
-ok 'got rust msg' 'grep -q "Received message: ich bin node" $TEMP'
-ok 'got node.js msg' 'grep -q "got \"ich bin rust\"" $TEMP'
+ok 'run command' 'npx dupsh -- "./cable.rs/target/release/examples/cli 11111111111111111111111111111111 responder --file 11.txt" "node ../../dist/examples/cli 3131313131313131313131313131313131313131313131313131313131313131 initiator @6317.txt" 2> $TEMP'
+ok 'got rust msg' 'grep -q "generations of pirates, and I saw by their movements that they were" $TEMP'
+ok 'got node.js msg' 'grep -q "The Hatter looked at the March Hare, who had followed him" $TEMP'
+cp $TEMP log
 ok 'removed tempfile' 'rm $TEMP'
 ######################################################################
 
+
+######################################################################
+echo '# long msg exchange (Rust initiator)'
+######################################################################
+TEMP=$(mktemp)
+ok 'run command' 'npx dupsh -- "./cable.rs/target/release/examples/cli 11111111111111111111111111111111 initiator --file 11.txt" "node ../../dist/examples/cli 3131313131313131313131313131313131313131313131313131313131313131 responder @6317.txt" 2> $TEMP'
+ok 'got rust msg' 'grep -q "generations of pirates, and I saw by their movements that they were" $TEMP'
+ok 'got node.js msg' 'grep -q "The Hatter looked at the March Hare, who had followed him" $TEMP'
+cp $TEMP log
+ok 'removed tempfile' 'rm $TEMP'
+######################################################################
