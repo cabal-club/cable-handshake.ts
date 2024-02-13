8ecab3c2b96f51a7d336608a3310eb00b8277375
Kira Oakley <kira@eight45.net>
2024-02-13 09:52:14 -0800

    Add 'test-integration' npm script.

--- test/integration/t003-long.sh
+++ test/integration/t003-long.sh
@@ -7,7 +7,6 @@ TEMP=$(mktemp)
 ok 'run command' 'npx dupsh -- "./cable.rs/target/release/examples/cli 11111111111111111111111111111111 responder --file 11.txt" "node ../../dist/examples/cli 3131313131313131313131313131313131313131313131313131313131313131 initiator @6317.txt" 2> $TEMP'
 ok 'got rust msg' 'grep -q "generations of pirates, and I saw by their movements that they were" $TEMP'
 ok 'got node.js msg' 'grep -q "The Hatter looked at the March Hare, who had followed him" $TEMP'
-cp $TEMP log
 ok 'removed tempfile' 'rm $TEMP'
 ######################################################################
 
@@ -19,6 +18,5 @@ TEMP=$(mktemp)
 ok 'run command' 'npx dupsh -- "./cable.rs/target/release/examples/cli 11111111111111111111111111111111 initiator --file 11.txt" "node ../../dist/examples/cli 3131313131313131313131313131313131313131313131313131313131313131 responder @6317.txt" 2> $TEMP'
 ok 'got rust msg' 'grep -q "generations of pirates, and I saw by their movements that they were" $TEMP'
 ok 'got node.js msg' 'grep -q "The Hatter looked at the March Hare, who had followed him" $TEMP'
-cp $TEMP log
 ok 'removed tempfile' 'rm $TEMP'
 ######################################################################
